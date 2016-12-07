848fbf135a9e7f6c05777e298118c82e9b10ee1c
pablo <pfi_23@outlook.com>
2016-12-06 23:08:11 -0600

    Se va a asignar color de  directorios rojo y de archivos negro

---
+++ src/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.io.File;
+
+public class Main {
+
+	
+	public static void main(String[] args) { 
+		
+		File archivo1;
+		File archivo2;
+		
+		archivo1 = new File("C:/Users/Gerardo Shimokawa/Desktop/JQuery");
+		
+		String[] nombres1 = archivo1.list();
+		
+		for (int i = 0; i < nombres1.length; i++) {
+			archivo2 =new File(archivo1,nombres1[i]);
+			if(archivo2.isDirectory()){
+				System.err.println(nombres1[i]);
+				String[] nombres2 = archivo2.list();
+				for (int j = 0; j < nombres2.length; j++) {
+					System.out.println("    "+nombres2[j]);
+				}
+			}else{
+				System.out.println(nombres1[i]);
+			}
+		}
+	}
+
+}
